e42af42dd76d343cc08468de31b54d3104d16bd7
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-10 17:12:23 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,17 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message: Message):
+    btn = [
+        [InlineKeyboardButton("➕ Add to Group", url=f"https://t.me/{client.me.username}?startgroup=true")],
+        [InlineKeyboardButton("🔥 Trending", callback_data="trending"),
+         InlineKeyboardButton("⚡ Upgrade", callback_data="upgrade")],
+        [InlineKeyboardButton("💸 Start Earning", callback_data="earning")]
+    ]
+    await message.reply_text(
+        "ʜᴇʏ Sandy ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ,\n\n"
+        "ɪ'ᴍ ᴀ ꜰᴀꜱᴛ & ꜰᴜʟʟ ᴄᴜꜱᴛᴏᴍɪᴢᴀʙʟᴇ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ᴡɪᴛʜ ᴀᴅᴠᴀɴᴄᴇ ꜰᴇᴀᴛᴜʀᴇꜱ. "
+        "ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴇᴀʀɴ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏɴᴇʏ...💸",
+        reply_markup=InlineKeyboardMarkup(btn)
+    )
