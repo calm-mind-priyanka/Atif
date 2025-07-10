b3cdac49dbe147c4e54943367668dded863fc4f3
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-10 19:18:51 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,7 +7,23 @@ def get_user_settings(user_id):
         "delete_time": "2m",
         "result_mode": "button",
         "caption": None,
-        "shortlinks": {"1": "", "2": ""},
-        "file_secure": False
+        "shortlinks": {"1": "", "2": ""},  # up to 2 shorteners
+        "file_secure": False,
+
+        # These were missing:
+        "force_channels": [],
+        "max_results": 5,
+        "imdb": False,
+        "file_mode": {
+            "type": "verify",  # or shortlink
+            "second_verify": False,
+            "verify_time": "300",  # in seconds
+            "log_channel": ""
+        },
+        "tutorial_links": {
+            "first": "",
+            "second": ""
+        },
+        "awaiting_input": None
     }
     return user_settings.setdefault(user_id, default)
