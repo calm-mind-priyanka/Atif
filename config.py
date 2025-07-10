a6000b1ebc3bb8a327094bc06e69054bd39ddf4b
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-10 16:56:53 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Hardcoded Configuration
+
+API_ID = 12345678
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+BOT_OWNER = [6046055058]  # Your Telegram user ID(s)
+
+MONGO_DB_URI_1 = "mongodb+srv://your_primary_uri"
+MONGO_DB_URI_2 = "mongodb+srv://your_secondary_uri"
+LOG_CHANNEL = -1001234567890
