8e033ee6dd419106225f5e74e43dbcf0f563f4cc
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-10 19:47:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import requests
+
 def readable_size(size_in_bytes):
     if size_in_bytes < 1024:
         return f"{size_in_bytes} B"
@@ -7,3 +9,17 @@ def readable_size(size_in_bytes):
         return f"{size_in_bytes / (1024 ** 2):.2f} MB"
     else:
         return f"{size_in_bytes / (1024 ** 3):.2f} GB"
+
+def fetch_imdb_details(title):
+    url = f"https://www.omdbapi.com/?t={title}&apikey=your_omdb_api_key"
+    res = requests.get(url).json()
+    if res["Response"] == "True":
+        return {
+            "title": res["Title"],
+            "year": res["Year"],
+            "poster": res["Poster"],
+            "rating": res["imdbRating"],
+            "genre": res["Genre"],
+            "plot": res["Plot"]
+        }
+    return None
