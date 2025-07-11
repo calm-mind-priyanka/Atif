04add5b844096b937432ad1ed3a0b2886c65653c
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-11 13:24:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ API_HASH = "your_api_hash"
 BOT_TOKEN = "your_bot_token"
 BOT_OWNER = [6046055058]  # Your Telegram user ID(s)
 
+# MongoDB URIs
 MONGO_DB_URI_1 = "mongodb+srv://your_primary_uri"
 MONGO_DB_URI_2 = "mongodb+srv://your_secondary_uri"
+
+# Toggle to use backup DB if primary fails
+USE_BACKUP_DB = True
+
+# Optional
 LOG_CHANNEL = -1001234567890
