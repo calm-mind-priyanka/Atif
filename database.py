92bc1770833798c66e7761b29e39fe9c75c3d25a
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2025-07-11 13:25:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,23 @@
 import motor.motor_asyncio
-from config import MONGO_DB_URI_1, MONGO_DB_URI_2
+from config import MONGO_DB_URI_1, MONGO_DB_URI_2, USE_BACKUP_DB
 
 client_1 = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI_1)
 client_2 = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI_2)
 
-db = client_1["autofilter"]
+db_primary = client_1["autofilter"]
 db_backup = client_2["autofilter"]
+
+
+def get_collection(name: str):
+    """
+    Returns a MongoDB collection from the primary DB.
+    If it fails and USE_BACKUP_DB is True, returns from backup DB.
+    """
+    try:
+        # Try accessing primary DB collection
+        _ = db_primary.list_collection_names()  # Trigger a request
+        return db_primary[name]
+    except Exception:
+        if USE_BACKUP_DB:
+            return db_backup[name]
+        raise
